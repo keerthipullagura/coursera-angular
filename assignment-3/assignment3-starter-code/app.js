40e2b05db918f64a1093305deb5f5d2a19e15227
keerthipullagura <kpullagura3@gatech.edu>
2023-05-28 11:16:57 -0700

    assignment3

--- assignment-3/assignment3-starter-code/app.js
+++ assignment-3/assignment3-starter-code/app.js
@@ -3,22 +3,42 @@
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItemsDirective)
         .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json");
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                items: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'ndc',
+            bindToController: true
+        };
+        return ddo;
+    }
+
 
     NarrowItDownController.$inject = ['MenuSearchService'];
 
     function NarrowItDownController(MenuSearchService) {
         var ndc = this;
         ndc.getMenuItems = function(searchTerm) {
-            ndc.foundItems = MenuSearchService.getMatchedMenuItems(searchTerm);
+            MenuSearchService.getMatchedMenuItems(searchTerm);
         }
+        ndc.items = MenuSearchService.getItems();
+        ndc.onRemove = function(itemIndex) {
+            MenuSearchService.removeItem(itemIndex);
+        };
     }
 
     MenuSearchService.$inject = ['$http', 'ApiBasePath']
 
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
-        var foundItems = [];
+        var items = [];
         service.getMatchedMenuItems = function(searchTerm) {
             var promise = $http({
                 method: 'GET',
@@ -31,19 +51,23 @@
                     var m_items = menus[menu].menu_items;
                     for (var index = 0; index < m_items.length; index++) {
                         if (m_items[index].description.includes(searchTerm)) {
-                            foundItems.push(m_items[index]);
+                            items.push(m_items[index]);
                         }
                     }
                 }
-                console.log(foundItems.length);
+                console.log(items.length);
             }).catch(function(error) {
                 console.log("Something went wrong.");
             });
-            return foundItems;
+            return items;
         }
 
-        service.getFoundItems = function() {
-            return foundItems;
+        service.getItems = function() {
+            return items;
+        }
+
+        service.removeItem = function(itemIndex) {
+            items.splice(itemIndex, 1);
         }
     }
 
